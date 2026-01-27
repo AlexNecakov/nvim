16165ee5ca152e7a23b3f8932c0c17d0902822bf
AlexNecakov <alexnecakov@gmail.com>
2026-01-27 08:49:08 -0500

    made run macro generic

--- lua/remap.lua
+++ lua/remap.lua
@@ -36,10 +36,15 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set("n", "<leader>b", ":!.\\build.bat", opts)
-vim.keymap.set("n", "<leader>B", ":!.\\build_release.bat", opts)
-vim.keymap.set("n", "<leader>r", ":!.\\build\\main", opts)
-
-vim.keymap.set("n", "<leader>db", ":!remedybg -g -q debugger.rdbg", opts)
+if vim.fn.has('win32') == 1 then
+    vim.keymap.set("n", "<leader>b", ":!.\\build.bat", opts)
+    vim.keymap.set("n", "<leader>B", ":!.\\build_release.bat", opts)
+    vim.keymap.set("n", "<leader>db", ":!remedybg -g -q debugger.rdbg", opts)
+    vim.keymap.set("n", "<leader>r", ":!.\\run.bat", opts)
+else
+    vim.keymap.set("n", "<leader>b", ":!.//build.sh", opts)
+    vim.keymap.set("n", "<leader>B", ":!.//build_release.sh", opts)
+    vim.keymap.set("n", "<leader>r", ":!.//run.sh", opts)
+end
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e " .. vim.fn.stdpath("config") .. "/lua/<cr>")
