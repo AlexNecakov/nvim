d658c41db19e7372063ee8428de59e8703cd390e
AlexNecakov <alexnecakov@gmail.com>
2026-01-28 08:47:21 -0500

    added sourcekit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,7 +38,7 @@ return {
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "ge", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("n", "gR", function() vim.lsp.buf.rename() end, opts)
             vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
 
@@ -83,6 +83,16 @@ return {
                 end,
             },
         })
+        vim.lsp.config['sourcekit'] = {
+            capabilities = {
+                workspace = {
+                    didChangeWatchedFiles = {
+                        dynamicRegistration = true,
+                    },
+                },
+            },
+        }
+        vim.lsp.enable('sourcekit')
 
         local luasnip = require("luasnip")
         cmp.setup({
