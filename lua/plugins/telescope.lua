6f680153b6fc12fbbb4876c1920439ed6acc8999
Alex Necakov <alexnecakov@gmail.com>
2026-04-04 12:40:32 -0400

    flex telescope for high text size screen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,39 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
-
     dependencies = {
         'nvim-lua/plenary.nvim'
     },
-
     config = function()
-        require('telescope').setup({})
+        local telescope = require('telescope')
+
+        telescope.setup({
+            defaults = {
+                layout_strategy = 'flex',
+                layout_config = {
+                    flex = {
+                        flip_columns = 100,
+                        flip_lines = 20,
+                    },
+                    horizontal = {
+                        preview_width = 0.55,
+                        preview_cutoff = 80,
+                    },
+                    vertical = {
+                        preview_cutoff = 20,
+                        preview_height = 0.6,
+                    },
+                    width = 0.85,
+                    height = 0.85,
+                },
+                preview = {
+                    hide_on_startup = false,
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
+
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
