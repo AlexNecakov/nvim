a6083f1b85902b177a385724506f1c0cde3f18db
AlexNecakov <alexnecakov@gmail.com>
2026-06-23 07:07:11 -0400

    psycho fixes

--- lua/remap.lua
+++ lua/remap.lua
@@ -22,7 +22,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("v", "<leader>s", [["zy:%s/\V<C-r>z/<C-r>z/gI<Left><Left><Left>]])
 
